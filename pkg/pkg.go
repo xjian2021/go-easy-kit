88c7f181425aae6f4dbb5cab81888281657a5057
xjian2021 <784093680@qq.com>
2021-12-30 21:59:49 +0800

    feat:新增判断字符串内包含大小写字母函数

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -13,3 +13,25 @@ func KindOfData(data interface{}) reflect.Kind {
 	}
 	return kind
 }
+
+//ContainsUpper 字符串中是否有大写字母
+func ContainsUpper(str string) bool {
+	for _, r := range str {
+		//大写字母ascii码值
+		if r > 64 && r < 91 {
+			return true
+		}
+	}
+	return false
+}
+
+//ContainsLower 字符串中是否有小写字母
+func ContainsLower(str string) bool {
+	for _, r := range str {
+		//小写字母ascii码值
+		if r > 96 && r < 123 {
+			return true
+		}
+	}
+	return false
+}
